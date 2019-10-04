64e032788aaacead919b9c08049a875f0c367ce3
wissal-mekki <48435711+wissal-mekki@users.noreply.github.com>
2019-10-04 01:46:38 +0100

    Update Micro2ApplicationTests.java

--- src/test/java/com/project/micro2/Micro2ApplicationTests.java
+++ src/test/java/com/project/micro2/Micro2ApplicationTests.java
@@ -1,16 +1,32 @@
 package com.project.micro2;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Micro2ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testConcatenation() {
+		String foo = "abc";
+		String bar = "def";
+		assertEquals("abcdef", foo + bar);
 	}
 
+	private void assertEquals(String abcdef, String s) {
+	}
+
+	@Test
+	public void testStartsWith() {
+		String foo = "abc";
+		assertTrue(foo.startsWith("ab"));
+
+	/*@Test
+	public void contextLoads() {
+	}*/
+	}
 }
